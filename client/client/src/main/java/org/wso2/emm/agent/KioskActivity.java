1a072331ab2207e95c1ced5df5dbca97769bb76c
Chatura Dilan <dilan@dilan.me>
2017-01-11 12:28:15 +0530

    Added COSU activity

---
+++ client/client/src/main/java/org/wso2/emm/agent/KioskActivity.java
@@ -0,0 +1,13 @@
+package org.wso2.emm.agent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KioskActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiosk);
+    }
+}

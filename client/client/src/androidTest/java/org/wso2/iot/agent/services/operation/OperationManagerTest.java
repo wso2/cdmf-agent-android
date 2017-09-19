ece19e738fcb91779374d67c42da995747880f04
Nirothipan <nirothipan@gmail.com>
2017-09-19 18:11:20 +0530

    code formatting

--- client/client/src/androidTest/java/org/wso2/iot/agent/services/operation/OperationManagerTest.java
+++ client/client/src/androidTest/java/org/wso2/iot/agent/services/operation/OperationManagerTest.java
@@ -19,7 +19,6 @@ package org.wso2.iot.agent.services.operation;
 
 import android.content.Context;
 import android.os.Environment;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
@@ -39,18 +38,19 @@ import org.wso2.iot.agent.services.operation.util.SFTPServer;
 import java.io.File;
 import java.io.IOException;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
 /**
  * Test class for OperationManager.java
  */
 @RunWith(AndroidJUnit4.class)
 public class OperationManagerTest {
-
     private static final String TAG = OperationManagerTest.class.getSimpleName();
     private String DEVICE_DOWNLOAD_DIRECTORY = Environment.getExternalStoragePublicDirectory
             (Environment.DIRECTORY_DOWNLOADS).toString();
     private String DOWNLOAD_FILE_NAME = "DownloadTestFile.txt";
     private String UPLOAD_FILE_NAME = "UploadTestFile.txt";
-    private Context CONTEXT = InstrumentationRegistry.getTargetContext();
+    private Context CONTEXT = getTargetContext();
     private String FTP_DIRECTORY = CONTEXT.getFilesDir().toString();
     private String SFTP_DIRECTORY = CONTEXT.getFilesDir().toString();
     private String USER_NAME = "test";
@@ -60,6 +60,9 @@ public class OperationManagerTest {
     private SFTPServer TEST_SFTP_SERVER;
     private int FTP_PORT = 2221;
     private int SFTP_PORT = 2223;
+    private String FTP_SCHEME = "ftp://";
+    private String SFTP_SCHEME = "sftp://";
+    private String LOCAL_HOST = "@localhost:";
 
     public void createNewFile(String location) {
         Log.d(TAG, "Creating new file (" + location + ") for testing .");
@@ -71,8 +74,16 @@ public class OperationManagerTest {
                 }
             }
         } catch (IOException e) {
-            Log.d(TAG, "Test file creation in failed.");
-            Assert.fail("Test file creation in device failed due to IO exception.");
+            Log.d(TAG, "Test file creation failed. " + e.getLocalizedMessage());
+            Assert.fail("Test file creation in device failed due to IO exception. " + e.getLocalizedMessage());
+        }
+    }
+
+    public void deleteFile(String location) {
+        Log.d(TAG, "Deleting file (" + location + ").");
+        File file = new File(location);
+        if (file.delete()) {
+            Log.d(TAG, "File deleted successfully.");
         }
     }
 
@@ -93,32 +104,32 @@ public class OperationManagerTest {
             payload.put("ftpPassword", password);
             payload.put("fileLocation", fileLocation);
         } catch (JSONException e) {
-            Log.e(TAG, "JSON Exception in preparing payload.");
+            Log.e(TAG, "JSON Exception in preparing payload. " + e.getLocalizedMessage());
         }
         return payload;
     }
 
     @Test
     public void testDownloadFileFTPClientFileLocationSpecified() {
-        String fileUrl = "ftp://" + USER_NAME + "@localhost:" + FTP_PORT + "/" + DOWNLOAD_FILE_NAME;
+        String fileUrl = FTP_SCHEME + USER_NAME + LOCAL_HOST + FTP_PORT + "/" + DOWNLOAD_FILE_NAME;
         testDownloadFile(preparePayload(fileUrl, PASSWORD, DEVICE_DOWNLOAD_DIRECTORY));
     }
 
     @Test
     public void testDownloadFileFTPClientFileLocationNotSpecified() {
-        String fileUrl = "ftp://" + USER_NAME + "@localhost:" + FTP_PORT + "/" + DOWNLOAD_FILE_NAME;
+        String fileUrl = FTP_SCHEME + USER_NAME + LOCAL_HOST + FTP_PORT + "/" + DOWNLOAD_FILE_NAME;
         testDownloadFile(preparePayload(fileUrl, PASSWORD, ""));
     }
 
     @Test
     public void testDownloadFileSFTPClientFileLocationSpecified() {
-        String fileUrl = "sftp://" + USER_NAME + "@localhost:" + SFTP_PORT + "/" + DOWNLOAD_FILE_NAME;
+        String fileUrl = SFTP_SCHEME + USER_NAME + LOCAL_HOST + SFTP_PORT + "/" + DOWNLOAD_FILE_NAME;
         testDownloadFile(preparePayload(fileUrl, PASSWORD, DEVICE_DOWNLOAD_DIRECTORY));
     }
 
     @Test
     public void testDownloadFileSFTPClientFileLocationNotSpecified() {
-        String fileUrl = "sftp://" + USER_NAME + "@localhost:" + SFTP_PORT + "/" + DOWNLOAD_FILE_NAME;
+        String fileUrl = SFTP_SCHEME + USER_NAME + LOCAL_HOST + SFTP_PORT + "/" + DOWNLOAD_FILE_NAME;
         testDownloadFile(preparePayload(fileUrl, PASSWORD, ""));
     }
 
@@ -130,7 +141,7 @@ public class OperationManagerTest {
         try {
             operationManager.downloadFile(operation);
         } catch (AndroidAgentException e) {
-            Log.e(TAG, "Android agent exception.");
+            Log.e(TAG, "Android agent exception." + e.getLocalizedMessage());
         }
         Log.d(TAG, "Operation status: \"" + operation.getStatus() + "\". Operation response: "
                 + operation.getOperationResponse());
@@ -139,14 +150,14 @@ public class OperationManagerTest {
 
     @Test
     public void testUploadFileFTPClient() {
-        String fileUrl = "ftp://" + USER_NAME + "@localhost:" + FTP_PORT + "/";
+        String fileUrl = FTP_SCHEME + USER_NAME + LOCAL_HOST + FTP_PORT + "/";
         String fileLocation = DEVICE_DOWNLOAD_DIRECTORY + File.separator + UPLOAD_FILE_NAME;
         testUploadFile(preparePayload(fileUrl, PASSWORD, fileLocation));
     }
 
     @Test
     public void testUploadFileSFTPClient() {
-        String fileUrl = "sftp://" + USER_NAME + "@localhost:" + SFTP_PORT + "/";
+        String fileUrl = SFTP_SCHEME + USER_NAME + LOCAL_HOST + SFTP_PORT + "/";
         String fileLocation = DEVICE_DOWNLOAD_DIRECTORY + File.separator + UPLOAD_FILE_NAME;
         testUploadFile(preparePayload(fileUrl, PASSWORD, fileLocation));
     }
@@ -155,14 +166,12 @@ public class OperationManagerTest {
     public void testUploadFile(JSONObject payload) {
         Log.d(TAG, "Testing for payload: " + payload);
         OperationManager operationManager = new MockOperationManager(CONTEXT);
-
         Operation operation = new Operation();
         operation.setPayLoad(payload);
-
         try {
             operationManager.uploadFile(operation);
         } catch (AndroidAgentException e) {
-            Log.e(TAG, "Android agent exception.");
+            Log.e(TAG, "Android agent exception. " + e.getLocalizedMessage());
         }
         Log.d(TAG, "Operation status: \"" + operation.getStatus() + "\". Operation response: "
                 + operation.getOperationResponse());
@@ -171,7 +180,13 @@ public class OperationManagerTest {
 
     @After
     public void stopServers() {
-        TEST_FTP_SERVER.stopFTP();
-        TEST_SFTP_SERVER.stopSFTP();
+        if (TEST_FTP_SERVER != null) {
+            TEST_FTP_SERVER.stopFTP();
+        }
+        if (TEST_SFTP_SERVER != null) {
+            TEST_SFTP_SERVER.stopSFTP();
+        }
+        deleteFile(FTP_DIRECTORY + File.separator + DOWNLOAD_FILE_NAME);
+        deleteFile(DEVICE_DOWNLOAD_DIRECTORY + File.separator + UPLOAD_FILE_NAME);
     }
 }

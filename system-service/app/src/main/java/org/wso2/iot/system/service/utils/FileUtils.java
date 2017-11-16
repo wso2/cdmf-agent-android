781df2ff74a4aea1cc009bfcef43754724d60f33
madhawap <madhawa.818@gmail.com>
2017-11-16 10:01:54 +0530

    Change Requests fixes to Resume interrupted download of over-the-air (OTA)

--- system-service/app/src/main/java/org/wso2/iot/system/service/utils/FileUtils.java
+++ system-service/app/src/main/java/org/wso2/iot/system/service/utils/FileUtils.java
@@ -36,8 +36,12 @@ public class FileUtils {
     public static String getUpgradePackageFilePath() {
         String path = getUpgradePackageDirectory();
         Log.d(FileUtils.class.getName(), path + File.separator + Constants.UPDATE_PACKAGE_NAME);
-        return getUpgradePackageDirectory() + File.separator + Constants.UPDATE_PACKAGE_NAME;
-//        return getUpgradePackageDirectory() + File.separator + "ota" + File.separator + Constants.UPDATE_PACKAGE_NAME;
+//        return getUpgradePackageDirectory() + File.separator + Constants.UPDATE_PACKAGE_NAME;
+        return getUpgradePackageDirectory() + File.separator + "ota" + File.separator + Constants.UPDATE_PACKAGE_NAME;
+    }
 
+    public static String getOTAPackageFilePath() {
+        String path = getUpgradePackageDirectory();
+        return getUpgradePackageDirectory() + File.separator + "ota";
     }
 }

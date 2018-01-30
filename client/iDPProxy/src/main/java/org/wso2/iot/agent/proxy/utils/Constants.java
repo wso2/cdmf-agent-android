d04aba57c903b1ad5fecaf488bae3ba80105f498
charitha <charitha.ws@gmail.com>
2018-01-30 14:57:42 +0530

    Fix https://github.com/wso2/product-iots/issues/1641

--- client/iDPProxy/src/main/java/org/wso2/iot/agent/proxy/utils/Constants.java
+++ client/iDPProxy/src/main/java/org/wso2/iot/agent/proxy/utils/Constants.java
@@ -98,6 +98,7 @@ public class Constants {
 	public final static String CLIENT_SECRET = "client_secret";
 	public final static String TOKEN_ENDPOINT = "token_endpoint";
 	public static enum HTTP_METHODS{GET, POST, DELETE, PUT};
+	public static final String CLIENT_ERROR = "-1";
 	public static final String INTERNAL_SERVER_ERROR = "500";
 	public static final String ACCESS_FAILURE = "400";
 	public static final String REQUEST_SUCCESSFUL = "200";
